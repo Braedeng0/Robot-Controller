ac2d4dac8774539dc076394c0548c876baea5a2c
Braedeng0 <115361091+Braedeng0@users.noreply.github.com>
2024-01-31 07:10:44 -0700

    Changed airplane toggle function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AirplaneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AirplaneLauncher.java
@@ -6,21 +6,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class AirplaneLauncher {
     private static final double airplaneReady = .175;
     private static final double airplaneLaunch = 1;
+    private boolean airplaneToggle = false;
     private Servo airplaneServo;
 
     public AirplaneLauncher(HardwareMap hardwareMap) {
         airplaneServo = hardwareMap.servo.get("airplane_servo");
+        ready();
     }
 
     public void ready() {airplaneServo.setPosition(airplaneReady);}
 
     public void launch() {airplaneServo.setPosition(airplaneLaunch);}
 
-    public void toggle() {
-        if (airplaneServo.getPosition() == airplaneReady){
-            airplaneServo.setPosition(airplaneLaunch);
-        } else {
-            airplaneServo.setPosition(airplaneReady);
-        }
-    }
+    public void toggle() {if (!airplaneToggle) launch(); else ready();}
 }
